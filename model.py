3b7dae5e5715de429a106d19c665eabd8e3764d2
xavier <xbarr@unm.edu>
2024-04-21 20:33:09 -0600

    initial commit

---
+++ model.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import torchvision
+
+class MnistClassifier(nn.Module):
+  def __init__(self):
+    super(MnistClassifier, self).__init__()
+    self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+    self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+    self.conv2_drop = nn.Dropout2d()
+    self.fc1 = nn.Linear(320, 50)
+    self.fc2 = nn.Linear(50, 10)
+
+  def forward(self, x):
+    x = nn.functional.relu(nn.functional.max_pool2d(self.conv1(x), 2))
+    x = nn.functional.relu(nn.functional.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+    x = x.view(-1, 320)
+    x = nn.functional.relu(self.fc1(x))
+    x = nn.functional.dropout(x, training=self.training)
+    x = self.fc2(x)
