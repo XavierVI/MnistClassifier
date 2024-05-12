64d70dd867203e6b194019610915dc58b6e149be
xavier <vigenesisvi@gmail.com>
2024-05-11 18:11:25 -0600

    changed name of the notebook and started writing code for training a model

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import torchvision
-import os
 
 """
 Notes: pixel size is 28x28 = 784
@@ -16,7 +15,27 @@ class MnistClassifier(nn.Module):
     self.linear_stack = nn.Sequential(
       nn.Linear(pixel_size, 128, bias=False),
       nn.Linear(128, 32, bias=False),
-      nn.Linear(32, 1, bias=False)
+      nn.Linear(32, 10, bias=False)
+    )
+  
+  def forward(self, x):
+    x = self.flatten(x) # flatten inputs
+    prediction = self.linear_stack(x) # make prediction
+    return prediction
+  
+class NonLinearClassifier(nn.Module):
+  def __init__(self):
+    super().__init__()
+    pixel_size = 784
+    # transforms each 2D (28x28) image into an array of 784 pixel values
+    self.flatten = nn.Flatten()
+    # nn.Sequential is an ordered container of modules
+    self.linear_stack = nn.Sequential(
+      nn.Linear(pixel_size, 128, bias=False),
+      nn.ReLU(),
+      nn.Linear(128, 32, bias=False),
+      nn.ReLU(),
+      nn.Linear(32, 10, bias=False)
     )
   
   def forward(self, x):
